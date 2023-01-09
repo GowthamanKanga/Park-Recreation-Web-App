744d9cea1305d695deda34304652c174d0ccf541
GowthamanKanga <83148850+GowthamanKanga@users.noreply.github.com>
2023-01-09 01:36:49 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './HomePages/Home';
+import HomeHeader from "./HomeHeader"
+
+function App() {
+  return (
+    <>
+    <HomeHeader/>
+    <Home/>
+    </>
+  );
+}
+
+export default App;
