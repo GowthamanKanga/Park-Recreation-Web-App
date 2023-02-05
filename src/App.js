d5e28564e4afd1df84f8de1adb1b202309231a5d
GowthamanKanga <83148850+GowthamanKanga@users.noreply.github.com>
2023-02-05 03:32:57 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import EventList from './pages/EventList';
+import FacilityList from './pages/FacilityList';
+import Home from './pages/Home';
+import ParkList from './pages/ParkList';
+import ParkMap from './pages/ParkMap';
+import BookingPage from './pages/BookingPage'
+
+
+
+function App() {
+  return (
+    <div>
+      <ParkMap/>
+    </div>
+  );
+}
+
+export default App;
