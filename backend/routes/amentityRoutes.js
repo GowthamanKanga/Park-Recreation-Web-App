7cf2e9b0f2f315a9c55c09da8e872c57e70cef84
GowthamanKanga <83148850+GowthamanKanga@users.noreply.github.com>
2023-02-07 22:54:37 -0500

    initial commit

---
+++ backend/routes/amentityRoutes.js
@@ -0,0 +1,137 @@
+const express = require('express')
+const route = express.Router()
+const equipment = require('../models/amentity')
+
+route.post('/amenities', async(req, res) => {
+    const newAmentity = req.body;
+    if(JSON.stringify(newAmentity) == null || JSON.stringify(newAmentity) == '{}') {
+        return res.status(400).send({
+            message: "facility's content can not be empty"
+        });
+    }
+    else {
+
+    try {
+        const item = new equipment(newAmentity)
+        await item.save()
+        res.status(201).send(item)
+    }
+    catch(error) {
+        res.status(500).send(error)
+    }
+}
+});
+
+route.get('/amenities', async(req, res) => {
+    try {
+        const amentities = await equipment.find({})
+        res.status(200).send(amentities)
+    }
+    catch(error) {
+        res.status(500).send(error)
+    }
+});
+route.get('/amenities/', async(req, res) => {
+    let keyword = req.query.keyword
+
+    if(JSON.stringify(keyword) == null || JSON.stringify(keyword) == '{}') {
+        return res.status(400).send({
+            message: "Facility's keyword can not be empty"
+        });
+    }
+    else {
+    try {
+        const amentities = await equipment.find({ $or: [{facilityName: `/^${keyword} `}, {facilityName: `/${keyword} $/`}, {facilityName: `/ ${keyword} /`}
+    , {facilityName: `/^${keyword}`}, {facilityName: `${keyword}$/`}, {facilityName: `/${keyword}/`}]})
+        res.status(200).send(amentities)
+    }
+    catch(error) {
+        res.status(500).send(error)
+    }
+}
+});
+
+
+route.get('/amenities/:id', async(req, res) => {
+
+    let id = req.params.id
+    if(JSON.stringify(id) == null || JSON.stringify(id) == '{}') {
+        return res.status(400).send({
+            message: "Facility content can not be empty"
+        });
+    }
+    else {
+    
+    try {
+        const item = await equipment.findById(id)
+        res.status(200).send(item)
+    }
+    catch(error) {
+        res.status(500).send(error)
+    }
+}
+});
+
+
+route.put('/amenities/:id', async(req, res) => {
+
+    let id = req.params.id
+    if(JSON.stringify(id) == null || JSON.stringify(id) == '{}') {
+        return res.status(400).send({
+            message: "Facilities's content can not be empty"
+        });
+    }
+    else {
+    try {
+        console.log(req.body)
+        const updatedInventory = await equipment.findByIdAndUpdate(id, req.body)
+    
+        await updatedInventory.save()
+        res.status(202).send(updatedInventory)
+      } catch (err) {
+        res.status(500).send(err)
+      }
+    }
+});
+
+route.patch('/amentities/:id', async (req, res) => {
+    try {
+      const amentity = await Amentity.findById(req.params.id)
+      if (!amentity) {
+        return res.status(404).send()
+      }
+      amentity.occupiedAmount = req.body.occupiedAmount
+      await amentity.save()
+      res.send(amentity)
+    } catch (e) {
+      res.status(400).send(e)
+    }
+  })
+  
+
+
+route.delete('/amenities/:id', async (req, res) => {
+    // Validate request
+    let id = req.params.id
+    if(JSON.stringify(id) == null || JSON.stringify(id) == '{}') {
+        return res.status(400).send({
+            message: "Facilities's content can not be empty"
+        });
+    }
+
+    else {
+    try {
+        const facility = await equipment.findByIdAndDelete(id)
+    
+        if (!facility) { 
+            res.status(404).send("No item found")
+        }
+        res.status(204).send(facility)
+      } catch (err) {
+        res.status(500).send(err)
+      }
+    }
+});
+module.exports = route
+
+
---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import EventList from './pages/EventList';
+import FacilityList from './pages/FacilityList';
+import Home from './pages/Home';
+import ParkList from './pages/ParkList';
+import ParkMap from './pages/ParkMap';
+import BookingPage from './pages/BookingPage'
+import { BrowserRouter as Router, Route, Routes, } from "react-router-dom";
+
+
+
+
+function App() {
+  return (
+    <>
+      <BookingPage/>
+    </>
+  );
+}
+
+export default App;
+
+
+{/* <Router>
+<Routes>
+  <Route path='/' element={<Home/>}/>
+  <Route path='/ParkList' element={<ParkList/>}/>
+  <Route path='/EventList' element={<EventList/>}/>
+  <Route path='/FacilityList' element={<FacilityList/>}/>
+  <Route path='/ParkMap' element={<ParkMap/>}/>
+  <Route path='/ParkMap' element={<ParkMap/>}/>
+  <Route path='/BookingPage' element={<BookingPage/>}/>
+</Routes>
+</Router> */}
